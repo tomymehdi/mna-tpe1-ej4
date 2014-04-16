5c9f99877d7a84f4b4256bde68842fce10bb204c
Tomas <tomymehdi@gmail.com>
2014-04-16 01:44:26 -0300

    merged with qr

---
+++ src/ourQR.m
@@ -0,0 +1,13 @@
+function [Q R] = ourQR (A)
+   [m n]=size(A);
+    Q(:,1)=A(:,1);
+    Q(:,1)=Q(:,1)/norm(Q(:,1),2);
+    for i=2:n
+        Q(:,i)=A(:,i);
+        for k=1:i-1
+            Q(:,i)=Q(:,i)-((A(:,i))'*Q(:,k))*Q(:,k);
+        endfor
+        Q(:,i)=Q(:,i)/norm(Q(:,i),2);
+    endfor
+    R= (Q')*A;
+endfunction
